6dd8097939f59b5d27e7dd86312817281cd5d074
Seth Carney <seth.carney@eloqua.com>
2012-02-16 17:54:39 -0500

    update to add column trimming

--- Rest.Sql/Controllers/QueryController.cs
+++ Rest.Sql/Controllers/QueryController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Newtonsoft.Json;
@@ -19,21 +20,42 @@ namespace Rest.Sql.Controllers
 
             var page = query.Page ?? 1;
             var pageSize = query.PageSize ?? 25;
+            var columns = query.GetColumns();
 
             var model = CreateDynamicModel(table);
 
-            var everything = model.All(query.Where, query.OrderBy);
+            var everything = model.All(query.Where, query.OrderBy, columns: columns);
+            // ReSharper disable PossibleMultipleEnumeration
             var total = everything.Count();
             var results = everything.Skip((page - 1) * pageSize).Take(pageSize);
+            // ReSharper restore PossibleMultipleEnumeration
 
-            var response = new {page, pageSize, total, query = new {table, query.Where, query.OrderBy}, results};
+            var response = CreateResponse(table, query, results, columns, page, pageSize, total);
             return JsonContent(response);
+        }
+
+        private static object CreateResponse(string table, QueryModel query, IEnumerable<dynamic> results, string columns, int page, int pageSize, int total)
+        {
+            return new
+                       {
+                           page,
+                           pageSize,
+                           total,
+                           query = new
+                                       {
+                                           table,
+                                           columns,
+                                           where = query.Where ?? string.Empty,
+                                           orderby = query.OrderBy ?? string.Empty
+                                       },
+                           results
+                       };
         }
 
         private ContentResult JsonContent(object model)
         {
             var serialized = JsonConvert.SerializeObject(model, Formatting.None);
-            return Content(serialized);
+            return Content(serialized, "applicaion/javascript");
         }
 
         private static DynamicModel CreateDynamicModel(string table)
--- Rest.Sql/Models/QueryModel.cs
+++ Rest.Sql/Models/QueryModel.cs
@@ -6,8 +6,15 @@
 
         public int? PageSize { get; set; }
 
+        public string Columns { get; set; }
+
         public string Where { get; set; }
 
         public string OrderBy { get; set; }
+
+        public string GetColumns()
+        {
+            return (string.IsNullOrWhiteSpace(Columns)) ? "*" : Columns;
+        }
     }
 }
